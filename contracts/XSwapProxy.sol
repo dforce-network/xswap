a19bd873b28f64f552551c47160baf9745e657e4
SnowJi <sj@mac.com>
2020-03-08 20:23:01 +0800

    fix parameters.

--- contracts/XSwapProxy.sol
+++ contracts/XSwapProxy.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.4;
 import './upgradeability/AdminUpgradeabilityProxy.sol';
 
 contract XSwapProxy is AdminUpgradeabilityProxy {
-    bytes _data;
-    constructor(address _implementation) public AdminUpgradeabilityProxy(_implementation, msg.sender, _data) {
+    constructor(address _implementation, bytes memory _data) public AdminUpgradeabilityProxy(_implementation, msg.sender, _data) {
     }
 }
